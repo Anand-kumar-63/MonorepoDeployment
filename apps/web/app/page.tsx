f4f89509d12fc3fb8b6b1d28a871c70f9e5b9b17
Anand-Kumar-63 <anandks8013@gmail.com>
2026-07-04 05:15:07 +0530

    Fetch user:Issue resolved in Frontend

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
-import { prismaClient } from "@repo/db";
-export default async function Home() {
-  const user = prismaClient.user.findMany();
-  return <div>{JSON.stringify(user)}</div>;
-}
+  import "dotenv/config";
+  import { prismaClient } from "@repo/db";
+  export default async function Home() {
+    const user = await prismaClient.user.findMany();
+    return <div>{JSON.stringify(user)}</div>;
+  }
+
